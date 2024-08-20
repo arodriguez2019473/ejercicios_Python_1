08d1aff3b7f6776767d2b492bd0ecfaff0fa2b56
Auky Rodriguez <2473@gruporeal.ad>
2024-08-20 14:11:17 -0600

    se realizan algunos ejercicios

---
+++ ifelse.py
@@ -0,0 +1,78 @@
+    # Sentencia If
+
+# -----------------------------------------------
+
+print("-----------------------------------------------")
+print("-----------------------------------------------")
+
+if True:
+    print('!El bloque If se ejecutara')
+
+# --- este me imprimira que la sentencia es verdadera
+
+print("-----------------------------------------------")
+print("-----------------------------------------------")
+
+# -----------------------------------------------
+
+x = 5 
+
+if x > 4:
+    print("la contidicion era verdadera!") # ejecutara esta sentencia
+
+# --- si en dado caso el '>' signo este en el lado contrario '<' cuando nosotros ejecutemos este no imprimira y finalizara el programa
+# --- ya que esta no es verdadera y este toma que no es verdadera y lo cerrara
+
+print("-----------------------------------------------")
+print("-----------------------------------------------")
+
+    # Sentensia else
+# -----------------------------------------------
+
+if not True:
+    print('¡La sentencia If ejecutara!')
+
+else:
+    print('¡La sentencia Else se ejecutara!')
+
+# --- Este imprimira que la sentencia Else se ejecutara y de tal manera que se puede cambiar dependiendo a la opcion que se desea
+# --- realizar si tu le quitas el not se imprimira la sentencia if
+
+print("-----------------------------------------------")
+print("-----------------------------------------------")
+
+# -----------------------------------------------
+
+y = 3 
+
+if y > 4:
+    print ('¡Esto no lo imprimire!') # cuando digo esto es por que no lo voy a imprimir pero puedo cambiar los valores para que lo haga
+else:
+    print('¡La condicion no era verdadera!') # esta sentencia es la que ejecutare o imprimire
+
+
+# --- esta sentencia me dice si es cierto o falso dependiendo a lo que nosotros vamos a utilizar en dado caso si el simbolo '>' se 
+# --- cambia de lugar hacia '<' este imprimira lo de '¡Esto no lo imprimire!'
+
+print("-----------------------------------------------")
+print("-----------------------------------------------")
+
+    # Sentensia elfi
+# -----------------------------------------------
+
+z = 7
+
+if z > 8:
+    print('¡no imprimire esto!') # no ejecutare esta estancia y no me imprimira
+
+elif z > 5:
+    print('¡Yo voy a imprimir!') #esta es la sentencia que ejecutare en el momento
+
+elif z > 6:
+    print('¡yo no imprimire') # de igual modo no va a imprimir
+
+else:
+    print('¡no imprimire de igual modo!') # la sentencia no imprimira 
+
+# --- este imprimira la sentencia 'Yo voy a imprimir' si nosotros cambiamos el 7 a algun numero menor de 5 este imprimira el else
+# --- y si es mayor a 7 nos dara 'no imprimire esto'
--- test2.py
+++ test2.py
@@ -1,10 +1,3 @@
-a = int(input('ingrese el numero inicial:'))
-b = int(input('ingrese el numero a finalizar'))
-
-numero = {'numero1':a, 'numero2':b}
-
-if (numero):
-    for valor in range(a, b, c):
-
-
-
+nums = [4, 78, 9, 84]
+for n in nums:
+    print(n)
