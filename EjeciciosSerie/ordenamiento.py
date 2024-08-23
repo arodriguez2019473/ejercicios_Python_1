35e87822bd2fbb4760f468b11d42922b0ea5c2fb
Auky Rodriguez <2473@gruporeal.ad>
2024-08-23 16:05:36 -0600

    se termina el programa de ordenamiento

--- EjeciciosSerie/ordenamiento.py
+++ EjeciciosSerie/ordenamiento.py
@@ -1,3 +1,62 @@
+import random
+
+def generarListaRandom(longitud: int) -> list[int]:
+    output: list[int] = []
+
+    # llenar la lista
+
+    lista = []
+
+    print('ingrese un numero de datos')
+    agregar = int(input(''))
+
+    for paso in range(agregar + 1):
+        lista.append(random.randint(0,100))
+
+    print('Como quiere que se imprima')
+    print('a) de menor a mayor')
+    print('b) de mayor a menor')
+
+    orden = input('Ingrese a o b: ')
+        
+    #saber cuantos elementos hay en la lista
+    numElementos = len(lista)
+            
+
+    for paso in range (numElementos - 1):
+
+        for index in range(numElementos - 1 - paso):
+            intercambiarValores: bool = False
+            if orden == 'a':
+                intercambiarValores = lista[index] > lista[index+1]
+            else:
+                intercambiarValores = lista[index] < lista[index+1]
+                
+            if intercambiarValores == True:
+                contenedor = lista[index]
+                lista[index] = lista[index+1]
+                lista[index+1] = contenedor
+
+    print(lista)
+
+    print('===========')
+ 
+
+    # while i < agregar:
+        
+    #     i += 1
+
+    #     if i == agregar:
+            
+    #         lista.append(random.randint(0,100))
+
+    #         print(lista)
+        
+
+    
+
+    return output
+
 def bubleSort(lista: list[int]) -> list[int]:
     
     # ingresar los datos a lista 
@@ -10,43 +69,63 @@ def bubleSort(lista: list[int]) -> list[int]:
         elif numero:
             if numero.isalpha():
                 print('no es numero intenta poner nuevamente un valor numerico')
-            # elif numero.isspace():
-            #     print(f'has ingresado un {numero} ingresa un numero')
+            
             elif numero == ' ':
                 print('has agregado un espacio')
+            
             else:
                 print(numero)
                 lista.append(int(numero))
         else:
             print('no has ingresado ningun caracter')
 
+    print('Como quiere que se imprima')
+    print('a) de menor a mayor')
+    print('b) de mayor a menor')
+
+    orden = input('Ingrese a o b: ')
+        
     #saber cuantos elementos hay en la lista
     numElementos = len(lista)
+            
 
     #volver a iniciar el ordenamiento: tantas veces como numeros hay en la lista
     for paso in range (numElementos - 1):
 
         #recorrer la lista
         for index in range(numElementos - 1 - paso):
-            #obtener el valor actual y el valor siguiente
             #si el valor actual es mayor que el valor siguiente: hacer un cambio entre los dos
-            if lista[index] > lista[index+1]:
+            intercambiarValores: bool = False
+            if orden == 'a':
+                intercambiarValores = lista[index] > lista[index+1]
+            else:
+                intercambiarValores = lista[index] < lista[index+1]
+                
+            if intercambiarValores == True:
                 contenedor = lista[index]
                 lista[index] = lista[index+1]
                 lista[index+1] = contenedor
 
-    # verificar que no sean letras
+    print(lista)
 
-    
-        # lista = []
+    print('===========')
+ 
 
-        # while numero != 'c':
-                
-        #     numero.append = input("ingrese un numero")
-        #     if numero != 'c':
-        #         break
+
     return lista
+        
+print('Generar lista')   
+print('a)Si')
+print('b)No')
 
+respuesta = input('si o no: ')
 
+if respuesta == 'si':
+    generarListaRandom([])
 
-print(bubleSort([]))
+elif respuesta == 'no':    
+    lista = []
+    bubleSort([])
+
+else:
+    respuesta('bye')
